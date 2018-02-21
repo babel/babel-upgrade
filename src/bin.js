a8faea653701abcaee732d78c017f128a13aa97a
Yuta Hiroto <git@about-hiroppy.com>
2018-02-21 12:25:03 +0900

    Modify bin

---
+++ src/bin.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { isAcceptedNodeVersion, writePackageJSON, writeBabelRC } = require('.');
+const globby = require('globby');
+const cwd = process.cwd();
+
+if (!isAcceptedNodeVersion()) {
+  throw new Error("Babel 7 will only support Node 4 and higher");
+}
+
+// TOOD: allow passing a specific path
+(async () => {
+  // account for nested babelrc's
+  const paths = await globby(['**/.babelrc', '!./node_modules/**']);
+  const packages = await globby(['**/package.json', '!./node_modules/**']);
+
+  // if not a monorepo
+  if (packages.length === 1) {
+    if (paths.length > 1) {
+      console.log("We suggest using the new 'overrides' option instead of nested .babelrc's, can check out http://new.babeljs.io/docs/en/next/babelrc.html#overrides");
+    }
+    paths.forEach(p => writeBabelRC(p));
+  }
+})();
+
+// TOOD: allow passing a specific path
+writePackageJSON();
+// TODO: just do this automatically
+console.log("You'll need to re-run yarn or npm install");
