91337d8dad25652d783b136dd61e20414ae98491
Noah Lemen <noah.lemen@gmail.com>
2018-03-02 16:26:50 -0500

    handling for spec/loose default changes for template literals and class properties

--- src/upgradeConfig.js
+++ src/upgradeConfig.js
@@ -1,4 +1,6 @@
-const { presets: oldPresets, plugins: oldPlugins } = require('./packageData');
+const { presets: oldPresets, plugins: oldPlugins, pluginsNowSpecByDefault } = require('./packageData');
+
+const unhandledPluginsNowSpecByDefault = new Set(pluginsNowSpecByDefault);
 
 // TODO: fix all of this
 function changePresets(config) {
@@ -68,19 +70,19 @@ function changePlugins(config) {
 
         if (pluginsToReplace.includes(plugin[0])) {
           if (oldPlugins[plugin[0]]) {
-            plugin[0] = oldPlugins[plugin[0]];
+            plugins[i] = setLooseIfNeeded([oldPlugins[plugin[0]], plugin[1]]);
           } else {
             plugins.splice(i, 1);
             i--;
           }
         }
       } else {
-        if (plugin.indexOf('babel-plugin') !== 0 && plugin[0].indexOf('@babel/') !== 0) {
+        if (plugin.indexOf('babel-plugin') !== 0 && plugin.indexOf('@babel/') !== 0) {
           plugin = `babel-plugin-${plugin}`;
         }
         if (pluginsToReplace.includes(plugin)) {
           if (oldPlugins[plugin]) {
-            plugins[i] = oldPlugins[plugin];
+            plugins[i] = setLooseIfNeeded(oldPlugins[plugin]);
           } else {
             plugins.splice(i, 1);
             i--;
@@ -88,6 +90,30 @@ function changePlugins(config) {
         }
       }
     }
+  }
+}
+
+function buildPluginGithubUrl(plugin) {
+  return `https://github.com/babel/babel/tree/master/packages/${plugin.replace('@', '').replace('/', '-')}`;
+}
+
+function setLooseIfNeeded(plugin) {
+  if (Array.isArray(plugin)) {
+    if (pluginsNowSpecByDefault.has(plugin[0])) {
+      plugin[1] = plugin[1] || {};
+      if (!plugin[1].spec) {
+        Object.assign(plugin[1], { loose: true });
+      }
+      delete plugin[1].spec;
+    }
+    unhandledPluginsNowSpecByDefault.delete(plugin[0]);
+    return Object.keys(plugin[1]).length > 0 ? plugin : plugin[0];
+  } else {
+    if (pluginsNowSpecByDefault.has(plugin)) {
+      plugin = [plugin, { loose: true }];
+    }
+    unhandledPluginsNowSpecByDefault.delete(plugin);
+    return plugin;
   }
 }
 
@@ -103,6 +129,10 @@ module.exports = function upgradeConfig(config) {
       changePlugins(config.env[env]);
     });
   }
+
+  unhandledPluginsNowSpecByDefault.forEach(plugin => {
+    console.log(`\`${plugin}\` is now spec by default. For parity with Babel 6, use the \`loose\` flag (${buildPluginGithubUrl(plugin).concat('#loose')})`)
+  });
 
   return config;
 };
