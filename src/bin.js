e57598e968c738843c38080e7063d1c6bbc095c9
TechQuery <shiy007@qq.com>
2018-09-12 10:39:44 +0800

    [ Add ]  Make CLI simpler & stronger by Commander.js [ Add ]  Git precommit hook for testing

--- src/bin.js
+++ src/bin.js
@@ -1,22 +1,25 @@
-const path = require('path');
-const { isAcceptedNodeVersion, writePackageJSON, writeBabelRC, writeMochaOpts, installDeps } = require('.');
 const globby = require('globby');
+const Commander = require('commander');
+const {
+  isAcceptedNodeVersion, writePackageJSON, writeBabelRC, writeMochaOpts, installDeps
+} = require('.');
+
 
 if (!isAcceptedNodeVersion()) {
   throw new Error("Babel 7 will only support Node 4 and higher");
 }
 
+Commander
+  .usage('[options]')
+  .option('-w, --write',  'Write back updated configure files')
+  .option('-i, --install',  'Install all the updated dependencies')
+  .allowUnknownOption(false)
+  .parse( process.argv );
+
+
 async function hasFlow() {
   const flowConfigs = await globby(['**/.flowconfig', '!**/node_modules/**']);
   return flowConfigs.length > 0;
-}
-
-function parseOptions(args, availableOptions) {
-  return Object.entries(availableOptions)
-    .map(([option, flags]) => ({
-      [option]: args.some(arg => flags.includes(arg))
-    }))
-    .reduce((options, option) => Object.assign(options, option), {});
 }
 
 // TODO: allow passing a specific path
@@ -26,13 +29,9 @@ function parseOptions(args, availableOptions) {
   const packages = await globby(['**/package.json', '!**/node_modules/**']);
   const mochaOpts = await globby(['**/mocha.opts', '!**/node_modules/**']);
   const flow = await hasFlow();
-  const cliOptions = parseOptions(process.argv, {
-    installDeps: ['--install'],
-    write: ['--write', '-w']
-  })
   const upgradeOptions = {
     hasFlow: flow,
-    write: cliOptions.write
+    write: Commander.write
   };
 
   // if not a monorepo
@@ -41,21 +40,17 @@ function parseOptions(args, availableOptions) {
       console.log("We suggest using the new 'overrides' option instead of nested .babelrc's, can check out http://new.babeljs.io/docs/en/next/babelrc.html#overrides");
       console.log("");
     }
-    paths.forEach(p => writeBabelRC(p, upgradeOptions));
+
+    for (let path of paths)  writeBabelRC(path, upgradeOptions);
   }
 
-  mochaOpts.forEach(p => writeMochaOpts(p, upgradeOptions));
+  for (let path of mochaOpts)  writeMochaOpts(path, upgradeOptions);
 
   // TODO: allow passing a specific path
   await writePackageJSON(upgradeOptions);
 
-  // TODO: add smarter CLI option handling if we support more options
-  if (cliOptions.installDeps) {
-    if (cliOptions.write) {
-      console.log('Installing new dependencies');
-      await installDeps();
-    } else {
-      console.log('Run babel-upgrade with --write (or) -w and --install for it to install the newly added dependencies');
-    }
+  if (Commander.install && Commander.write) {
+    console.log('Installing new dependencies');
+    await installDeps();
   }
 })();
